6e36a20653a4b8d3fbb0a66e4f4a2ee1b4c07999
crodriguezuribe <crodriguezuribe@gmail.com>
2021-06-30 17:15:22 -0500

    3rd training day

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.math.BigInteger;
+
 public class App {
+    private String nombre;
+    private int edad;
+    private Double saldo_cuenta_bancaria;
+    private BigInteger numero_grande;
+    private boolean respuesta = false; 
+    // comentar esta linea
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        boolean respuesta = false; 
+        System.out.println(respuesta);
     }
 }
